9d7ef9c8ba918d6b1640f193d8f415ffedd6e478
Susana Lozano <susanalozanoadrio@gmail.com>
2017-09-17 00:21:42 +0200

    css responsive and js
    first delivery

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var button = document.querySelector(".btnOpenMenu");
+
+function myToggle() {
+  var mobileMenu = document.querySelector(".menu-mobile");
+  mobileMenu.classList.toggle("open-menu-mobile");
+}
+button.addEventListener("click",myToggle);
